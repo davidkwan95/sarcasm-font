9bbad2881a1d75710283649a0f18c23d0bf92de5
davidkwan95 <davidkwan95@gmail.com>
2019-10-29 21:32:22 +0700

    create basic alternating caps converter

---
+++ src/converter.ts
@@ -0,0 +1,17 @@
+function convertToAlternatingCase(text: string) {
+  const words = text.toLowerCase().split(" ");
+
+  const outputWords = words.map(w => {
+    let newWord = "";
+    for (let i = 0; i < w.length; i++) {
+      newWord += i % 2 == 0 ? w[i] : w[i].toUpperCase();
+    }
+
+    return newWord;
+  });
+
+  return outputWords.join(" ");
+}
+
+console.log(convertToAlternatingCase("make sarcasm with less effort now"));
+console.log(convertToAlternatingCase("people are gonna love this project"));
